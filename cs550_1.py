7e6fd9cb6fc89b0e48851a49c679f65baa9bf988
Esther <ean21@choate.edu>
2018-09-07 09:06:18 -0400

    Initial commit

---
+++ cs550_1.py
@@ -0,0 +1,8 @@
+# 09/07/2018, CS550 - Computer Science
+print ("Hello, world!")
+print ("hello leggers. are you okay")
+
+
+username = input("What is your name?" + "\n")
+print("Nice to meet you, " + username + "!")
+username = input("How was your day?")
