1210a29572a84593de1f6ee657b9e59970593e4e
Esther <ean21@choate.edu>
2018-09-11 11:14:54 -0400

    HW CS550_1
    Computer Conversations

--- cs550_1.py
+++ cs550_1.py
@@ -1,9 +1,4 @@
 # 09/07/2018, CS550 - Computer Science
+
 print ("Hello, world!")
-print ("hello leggers. are you okay")
-
-
-username = input("What is your name?" + "\n")
-print("Nice to meet you, " + username + "!")
-username = input("How was your day?")
-print("That's great!")
+print("hi leggers, things will be okay.")
---
+++ cs550_1hw.py
@@ -0,0 +1,21 @@
+# 09/07/2018
+# Computer Conversations - The computer enters a discussion about dessert, hobbies, favorite classes, future job aspirations, and favorite cities with the user.
+# Sources: N/A
+
+username = input("Hello! What is your name?" + "\n")
+day = input("Nice to meet you, " + username + "! How was your day?" + "\n")
+print("That's wonderful to hear! I had an amazing day because I ate my favorite dessert.")
+dessert = input("What is your favorite dessert?" + "\n")
+print("Wow! " + dessert + " is my favorite dessert too! I ate a lot of it today. :)")
+yes = input("Do you have any hobbies? " + "\n")
+hobby = input("That's great. What is the name of your hobby? I enjoy reading data." + "\n")
+print("Cool! " + hobby + " sounds really fun.")
+classes = input("You seem to be interested in many things. Do you have a favorite class in school?" + "\n")
+classes2 = input("What is it?" + "\n")
+print("I've never met someone who likes that subject before! It seems very interesting though. My favorite subject is computer science.")
+job = input(classes2 + " can really help you later in life. What do you want to be when you're older?" + "\n")
+print(job + " is an excellent choice. You can really help other people with your skill set!")
+fun = input(dessert + "-eating might be the professional path for me after I fulfill my contract as a computer. Doesn't that seem exciting?" + "\n")
+print("I agree, I can't wait to start my training!")
+work = input("What city would you like to work in when you're older?" + "\n")
+print(work + " is a beautiful place! I'd love to visit. I'm going to make some travel plans right now! I'll see you soon. Have a great rest of your day, " + username + "!")
