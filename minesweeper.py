d69814cef081fa9c038e0cfa595e4298d33008ea
Esther <ean21@choate.edu>
2018-10-04 10:51:50 -0400

    Minesweeper_3

--- minesweeper.py
+++ minesweeper.py
@@ -9,78 +9,74 @@ Sources:
 import sys
 import random as r
 
-w = int(sys.argv[1]) + 2
-h = int(sys.argv[2]) + 2
-B = int(sys.argv[3])
+w = None
+h = None
+B = None
 
-board = [[0 for a in range(w)] for b in range(h)]
+def startGame():
+	global w,h,B
 
-# place bombs
-for i in range(B):
-	b = r.randint(1,w-2)
-	a = r.randint(1,h-2)
-	if board[a][b] is not "*": # if not already a bomb
-		board[a][b] = "*"
+	print("\n\n\n#################\n" +
+		"Play Minesweeper!\n" +
+		"#################\n")
 
-		"""
-		p = [board[a-1][b-1], board[a-1][b], board[a-1][b+1], 
-		board[a][b-1], board[a][b+1],
-		board[a+1][b-1], board[a+1][b], board[a+1][b+1]]
+	# set dimensions for board
+	w = int(sys.argv[1]) + 2
+	h = int(sys.argv[2]) + 2
+	B = int(sys.argv[3])
 
-		for i in range(7):
-			if p[i] != "*":
-				p[i] += 1
-		"""
-
-		if board[a-1][b-1] != "*":
-			board[a-1][b-1] += 1
-		if board[a-1][b] != "*":
-			board[a-1][b] += 1
-		if board[a-1][b+1] != "*":
-			board[a-1][b+1] += 1
-
-		if board[a][b-1] != "*":
-			board[a][b-1] += 1
-		if board[a][b+1] != "*":
-			board[a][b+1] += 1
-		
-		if board[a+1][b-1] != "*":
-			board[a+1][b-1] += 1
-		if board[a+1][b] != "*":
-			board[a+1][b] += 1
-		if board[a+1][b+1] != "*":
-			board[a+1][b+1] += 1
-	
-
-""" Hiding items in a list
-a = [['A',True], ['B',True], ['C',True], ['D',True], ['E',True]]
-
-def show(index):
-    a[index][1] = True
-
-def hide(index):
-    a[index][1] = False
-
-def display():
-    print([x[0] for x in a if x[1]])
-"""
+	initBoard = [['X']*w for b in range(h)]
+	# print user board
+	for i in range(1,h-1):
+		for j in range(1,w-1):
+			# board[i][j]
+			print(initBoard[i][j], end=" ")
+		sys.stdout.write("\n")
 
 
-# print initial hidden board
-print("\n\n\n#################\n" +
-	"Play Minesweeper!\n" +
-	"#################\n")
+def endGame():
+	print("Sorry, you lost!")
 
-for i in range(1,h-1):
-	for j in range(1,w-1):
-		board
 
-# print board
-for i in range(1,h-1):
-	for j in range(1,w-1):
-		board[i][j]
-		print(board[i][j], end=" ")
-	sys.stdout.write("\n")
+def msGame():
+
+	# initial number of adjacent bombs set equal to 0
+	board = [[0 for a in range(w)] for b in range(h)]
+
+	# place bombs
+	for i in range(B):
+		b = r.randint(1,w-2)
+		a = r.randint(1,h-2)
+		if board[a][b] is not "*": # if not already a bomb
+			board[a][b] = "*"
+			# determining number of bombs adjacent to selected square
+			for i in range(-1,2):
+				for j in range(-1,2):
+					if board[a+j][b+i] != "*":
+						board[a+j][b+i] += 1
+
+
+	user = input("\nSelect position x,y: ")
+
+	""" code for revealing user position in progress
+	if board[a][b] != 'X':
+		if board[a][b] == '*':
+			endGame()
+		else:
+			initBoard[a][b] = board[a][b]
+	"""
+
+	# print full board
+	# len(board)-1 is height
+	for i in range(1,len(board)-1):
+		# len(board[0])-1 is width
+		for j in range(1,len(board[0])-1):
+			board[i][j]
+			print(board[i][j], end=" ")
+		sys.stdout.write("\n")
+
+startGame()
+msGame()
 
 
 
